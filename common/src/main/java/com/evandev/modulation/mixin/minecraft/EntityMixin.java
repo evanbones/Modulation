4de61b7c6e8b70998a8b933f59430927c3837f74
evanbones <74276616+evanbones@users.noreply.github.com>
2026-02-28 00:23:06 -0800

    Attack sleeping villagers tweak

---
+++ common/src/main/java/com/evandev/modulation/mixin/minecraft/EntityMixin.java
@@ -0,0 +1,25 @@
+package com.evandev.modulation.mixin.minecraft;
+
+import com.evandev.modulation.api.ModuleManager;
+import com.evandev.modulation.modules.VanillaModule;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.Villager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+
+    @Inject(method = "getPickRadius", at = @At("HEAD"), cancellable = true)
+    private void onGetPickRadius(CallbackInfoReturnable<Float> cir) {
+        if ((Object) this instanceof Villager villager && villager.isSleeping()) {
+            VanillaModule module = (VanillaModule) ModuleManager.getModule("vanilla");
+
+            if (module != null && module.isAttackSleepingVillagersEnabled()) {
+                cir.setReturnValue(0.5F);
+            }
+        }
+    }
+}
