4ae90fc9503703d9dc5d6d2a47417d765ab276d5
evanbones <74276616+evanbones@users.noreply.github.com>
2026-03-26 17:26:41 -0700

    Strip out music stuff

--- neoforge/src/main/java/com/evandev/modulation/client/ModulationClient.java
+++ neoforge/src/main/java/com/evandev/modulation/client/ModulationClient.java
@@ -1,31 +1,14 @@
 package com.evandev.modulation.client;
 
-import com.evandev.modulation.api.ModuleManager;
 import com.evandev.modulation.client.integration.ClothConfigIntegration;
-import com.evandev.modulation.modules.MusicModule;
-import com.evandev.modulation.modules.music.MusicClientLogic;
 import com.evandev.modulation.platform.Services;
-import net.minecraft.client.Minecraft;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.common.NeoForge;
 
 public class ModulationClient {
     public static void register(ModContainer container) {
         if (Services.PLATFORM.isModLoaded("cloth_config")) {
             container.registerExtensionPoint(IConfigScreenFactory.class, (mc, parent) -> ClothConfigIntegration.createScreen(parent));
-        }
-        NeoForge.EVENT_BUS.addListener(ModulationClient::onClientTick);
-    }
-
-    private static void onClientTick(ClientTickEvent.Post event) {
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.player != null) {
-            MusicModule module = (MusicModule) ModuleManager.getModule("music");
-            if (module != null && module.shouldLoad()) {
-                MusicClientLogic.getInstance().onClientTick(mc);
-            }
         }
     }
 }
